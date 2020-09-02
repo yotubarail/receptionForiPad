c9c35f4be352f5f68c96b06dd682e850d89cf452
Shota Takinami <shota_t@i.softbank.jp>
2020-09-02 09:55:06 +0900

    Initial Commit

---
+++ receptionForiPad/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  receptionForiPad
+//
+//  Created by 滝浪翔太 on 2020/09/02.
+//  Copyright © 2020 滝浪翔太. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
